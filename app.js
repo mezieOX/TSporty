a43e4975aef6364fa42f1406bf7b567699bf5d21
Ikemma3082 <80706383+Ikemma3082@users.noreply.github.com>
2022-08-30 06:59:51 -0700

    initial commit

--- app.js
+++ app.js
@@ -3,3 +3,21 @@ let home = document.querySelectorAll(".home");
 home.addEventListener("click", (e) => {
     e.preventDefault();
 })
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        600:{
+            slidesPerView:2,
+        },
+    },
+});
